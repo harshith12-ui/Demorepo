0d91512bf1cb8c74482bf42ec70d03569e1775df
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-10-26 16:14:56 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -1,2 +1,1 @@
 https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodCollection.aspx/CountApprovalLeftForApplicationCollection
-{"CurAppSel":"SATECH","AppShortNameArray":["cnt_TRA","cnt_BRQ","cnt_GCS","cnt_LMS","cnt_TAS","cnt_PIPB","cnt_SM","cnt_SR","cnt_SPS","cnt_TARMAC","cnt_ITIME","cnt_AAM","cnt_AAI","cnt_CO","cnt_CA","cnt_CVM","cnt_CCL","cnt_ACT","cnt_ESS","cnt_ESOP","cnt_EMS","cnt_FM","cnt_GIS","cnt_IRFS","cnt_IFC","cnt_ODC","cnt_PMS","cnt_PG","cnt_RAS","cnt_RFS","cnt_SAINFRA","cnt_SATECH","cnt_SMM","cnt_SSD","cnt_STA","cnt_TOSCANA","cnt_TMS","cnt_VCP"]}
