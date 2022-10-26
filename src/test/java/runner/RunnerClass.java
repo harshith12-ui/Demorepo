8044e04bef208c38de0bc1188700dd5cd545d8f1
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-10-26 14:40:44 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -11,7 +11,48 @@ import cucumber.api.junit.Cucumber;
 public class RunnerClass {
 	
 	Hi
-	
+	{
+    "d": {
+        "cnt_BRQ": "0",
+        "cnt_GCS": "0",
+        "cnt_LMS": "0",
+        "cnt_TAS": "0",
+        "cnt_PIPB": "0",
+        "cnt_SM": "0",
+        "cnt_SR": "0",
+        "cnt_SPS": "0",
+        "cnt_TARMAC": "0",
+        "cnt_ITIME": "0",
+        "cnt_AAM": "0",
+        "cnt_AAI": "0",
+        "cnt_CO": "0",
+        "cnt_CA": "0",
+        "cnt_CVM": "0",
+        "cnt_CCL": "0",
+        "cnt_ACT": "0",
+        "cnt_ESS": "0",
+        "cnt_ESOP": "0",
+        "cnt_EMS": "0",
+        "cnt_FM": "0",
+        "cnt_GIS": "0",
+        "cnt_IRFS": "0",
+        "cnt_IFC": "0",
+        "cnt_ODC": "0",
+        "cnt_PMS": "0",
+        "cnt_PG": "0",
+        "cnt_RAS": "0",
+        "cnt_RFS": "0",
+        "cnt_SAINFRA": "0",
+        "cnt_SATECH": "0",
+        "cnt_SMM": "0",
+        "cnt_SSD": "0",
+        "cnt_STA": "0",
+        "cnt_TOSCANA": "0",
+        "cnt_TMS": "0",
+        "cnt_VCP": "0",
+        "cnt_TRA": "122"
+    }
+}
 	
 	//it should Combine both feature file and stepdefination
 
