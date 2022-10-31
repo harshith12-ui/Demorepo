d65aed84f6059c209ce44d52a62e2c0f0cf6b712
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-10-31 11:48:34 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -1,2 +1,40 @@
 
 https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodCollection.aspx/CountApprovalLeftForApplicationColection
+
+"$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://example.com/example.json",
+    "type": "object",
+    "default": {},
+    "title": "Root Schema",
+    "required": [
+        "d"
+    ],
+    "properties": {
+        "d": {
+            "type": "object",
+            "default": {},
+            "title": "The d Schema",
+            "required": [
+                "0"
+            ],
+            "properties": {
+                "0": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The 0 Schema",
+                    "examples": [
+                        "0"
+                    ]
+                }
+            },
+            "examples": [{
+                "0": "0"
+            }]
+        }
+    },
+    "examples": [{
+        "d": {
+            "0": "0"
+        }
+    }]
+}
