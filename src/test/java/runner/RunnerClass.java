2ecf48eb5e664605033f18b79c12c2206075cafc
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-10-31 11:45:27 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -1,4 +1,2 @@
 
-https://myapprovalqa.eds-appase-dev.appserviceenvironment.net/Design/WebMethodCollection.aspx/CountApprovalLeftForApplicationColection
-{
-    JSONError: Unexpected token '<' at 1:1 <!DOCTYPE html> ^
+https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodCollection.aspx/CountApprovalLeftForApplicationColection
