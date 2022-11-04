3761c6290a6fff15fbb6b50d27762c5fae572e1b
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-11-04 13:08:39 +0530

    Update RunnerClass.java

--- src/test/java/runner/RunnerClass.java
+++ src/test/java/runner/RunnerClass.java
@@ -2,16 +2,15 @@ https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodC
 {"CurAppSel":"SAINFRA","AppShortNameArray":["cnt_TRA","cnt_BRQ","cnt_GCS","cnt_LMS","cnt_TAS","cnt_PIPB","cnt_SM","cnt_SR","cnt_SPS","cnt_TARMAC","cnt_ITIME","cnt_AAM","cnt_AAI","cnt_CO","cnt_CA","cnt_CVM","cnt_CCL","cnt_ACT","cnt_ESS","cnt_ESOP","cnt_EMS","cnt_FM","cnt_GIS","cnt_IRFS","cnt_IFC","cnt_ODC","cnt_PMS","cnt_PG","cnt_RAS","cnt_RFS","cnt_SAINFRA","cnt_SATECH","cnt_SMM","cnt_SSD","cnt_STA","cnt_TOSCANA","cnt_TMS","cnt_VCP"]}
 {
     "d": {
-        "cnt_TRA": "122",
-        "cnt_GCS": "9",
-        "cnt_TARMAC": "1",
         "cnt_BRQ": "0",
+        "cnt_GCS": "0",
         "cnt_LMS": "0",
         "cnt_TAS": "0",
         "cnt_PIPB": "0",
         "cnt_SM": "0",
         "cnt_SR": "0",
         "cnt_SPS": "0",
+        "cnt_TARMAC": "0",¬¬
         "cnt_ITIME": "0",
         "cnt_AAM": "0",
         "cnt_AAI": "0",
@@ -32,6 +31,7 @@ https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodC
         "cnt_PG": "0",
         "cnt_RAS": "0",
         "cnt_RFS": "0",
+        "cnt_SAINFRA": "0",
         "cnt_SATECH": "0",
         "cnt_SMM": "0",
         "cnt_SSD": "0",
@@ -39,7 +39,7 @@ https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodC
         "cnt_TOSCANA": "0",
         "cnt_TMS": "0",
         "cnt_VCP": "0",
-        "cnt_SAINFRA": "1"
-    }
-}
+        "cnt_TRA": "122"
+    
+
 Response time:7.41 s
