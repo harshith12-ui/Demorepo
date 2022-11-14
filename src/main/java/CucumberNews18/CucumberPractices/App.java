3e8ed161c6371d640378d52ac076769bd6352a85
Abhi1234567890123 <91111339+Abhi1234567890123@users.noreply.github.com>
2022-11-14 15:09:07 +0530

    Update App.java

--- src/main/java/CucumberNews18/CucumberPractices/App.java
+++ src/main/java/CucumberNews18/CucumberPractices/App.java
@@ -1,32 +1,488 @@
 {
-    "d": {
-        "cnt_BRQ": "0",
-        "cnt_CO": "0",
-        "cnt_ESS": "0",
-        "cnt_EMS": "0",
-        "cnt_GCS": "0",
-        "cnt_GIS": "0",
-        "cnt_IFC": "0",
-        "cnt_LMS": "0",
-        "cnt_TAS": "0",
-        "cnt_PMS": "0",
-        "cnt_PIPB": "0",
-        "cnt_RAS": "0",
-        "cnt_SM": "0",
-        "cnt_ACT": "0",
-        "cnt_SMM": "0",
-        "cnt_SR": "0",
-        "cnt_SPS": "0",
-        "cnt_STA": "0",
-        "cnt_TARMAC": "0",
-        "cnt_TOSCANA": "0",
-        "cnt_TMS": "0",
-        "cnt_ITIME": "0",
-        "cnt_TRA": "0",
-        "cnt_IRFS": "0",
-        "cnt_SATECH": "0",
-        "cnt_SAINFRA": "0",
-        "cnt_AAM": "5"
-    }
-https://myapprovalhcl.eds-appase-dev.appserviceenvironment.net/Design/WebMethodCollection.aspx/CountApprovalLeftForApplicationCollection
-    {"CurAppSel":"AAM","AppShortNameArray":["cnt_AAM","cnt_BRQ","cnt_CO","cnt_ESS","cnt_EMS","cnt_GCS","cnt_GIS","cnt_IFC","cnt_LMS","cnt_TAS","cnt_PMS","cnt_PIPB","cnt_RAS","cnt_SM","cnt_ACT","cnt_SMM","cnt_SR","cnt_SPS","cnt_STA","cnt_TARMAC","cnt_TOSCANA","cnt_TMS","cnt_ITIME","cnt_TRA","cnt_IRFS","cnt_SATECH","cnt_SAINFRA"]}
+ "d:  {
+"cnt_TRA": "45", 
+"cnt_STA": "32",
+"cnt_SAINFRA": "14",
+"cnt_GCS": "11" ,
+"cnt_GIS": "11",
+"cnt_SATECH": "8",
+"cnt_TAS": "7",
+"cnt_SR": "5",
+"cnt_TARMAC": "1",
+"cnt_LMS": "0",
+"cnt_SPS": "0",
+"cnt_PIPB": "0",
+"cnt_SM": "0",
+"cnt_AAM": "0",
+"cnt_AAI": "0",
+"cnt_CO": "0",
+"cnt_CA": "0",
+"cnt_CVM": "0",
+"cnt_CCL": "0",
+"cnt_ACT": "0",
+"cnt_ESS": "0",
+"cnt_ESOP": "0",
+"cnt_EMS": "0",
+"cnt_FM": "0",
+"cnt_IRFS": "0",
+"cnt_IFC": "0",
+"cnt_ODC": "0",
+"cnt_PMS": "0",
+"cnt_PG": "0",
+"cnt_RAS": "0",
+"cnt_RFS": "0",
+"cnt_SMM": "0",
+"cnt_SSD": "0",
+"cnt_TOSCANA": "0",
+"cnt_TMS": "0",
+"cnt_ITIME": "0",
+"cnt_VCP": "0",
+"cnt_BRQ": "0"
+}
+}
+
+{
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$id": "http://example.com/example.json",
+    "type": "object",
+    "default": {},
+    "title": "Root Schema",
+    "required": [
+        "d"
+    ],
+    "properties": {
+        "d": {
+            "type": "object",
+            "default": {},
+            "title": "The d Schema",
+            "required": [
+                "cnt_TRA",
+                "cnt_STA",
+                "cnt_SAINFRA",
+                "cnt_GCS",
+                "cnt_GIS",
+                "cnt_SATECH",
+                "cnt_TAS",
+                "cnt_SR",
+                "cnt_TARMAC",
+                "cnt_LMS",
+                "cnt_SPS",
+                "cnt_PIPB",
+                "cnt_SM",
+                "cnt_AAM",
+                "cnt_AAI",
+                "cnt_CO",
+                "cnt_CA",
+                "cnt_CVM",
+                "cnt_CCL",
+                "cnt_ACT",
+                "cnt_ESS",
+                "cnt_ESOP",
+                "cnt_EMS",
+                "cnt_FM",
+                "cnt_IRFS",
+                "cnt_IFC",
+                "cnt_ODC",
+                "cnt_PMS",
+                "cnt_PG",
+                "cnt_RAS",
+                "cnt_RFS",
+                "cnt_SMM",
+                "cnt_SSD",
+                "cnt_TOSCANA",
+                "cnt_TMS",
+                "cnt_ITIME",
+                "cnt_VCP",
+                "cnt_BRQ"
+            ],
+            "properties": {
+                "cnt_TRA": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_TRA Schema",
+                    "examples": [
+                        "45"
+                    ]
+                },
+                "cnt_STA": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_STA Schema",
+                    "examples": [
+                        "32"
+                    ]
+                },
+                "cnt_SAINFRA": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SAINFRA Schema",
+                    "examples": [
+                        "14"
+                    ]
+                },
+                "cnt_GCS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_GCS Schema",
+                    "examples": [
+                        "11"
+                    ]
+                },
+                "cnt_GIS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_GIS Schema",
+                    "examples": [
+                        "11"
+                    ]
+                },
+                "cnt_SATECH": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SATECH Schema",
+                    "examples": [
+                        "8"
+                    ]
+                },
+                "cnt_TAS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_TAS Schema",
+                    "examples": [
+                        "7"
+                    ]
+                },
+                "cnt_SR": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SR Schema",
+                    "examples": [
+                        "5"
+                    ]
+                },
+                "cnt_TARMAC": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_TARMAC Schema",
+                    "examples": [
+                        "1"
+                    ]
+                },
+                "cnt_LMS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_LMS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_SPS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SPS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_PIPB": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_PIPB Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_SM": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SM Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_AAM": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_AAM Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_AAI": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_AAI Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_CO": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_CO Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_CA": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_CA Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_CVM": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_CVM Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_CCL": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_CCL Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_ACT": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_ACT Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_ESS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_ESS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_ESOP": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_ESOP Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_EMS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_EMS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_FM": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_FM Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_IRFS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_IRFS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_IFC": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_IFC Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_ODC": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_ODC Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_PMS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_PMS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_PG": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_PG Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_RAS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_RAS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_RFS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_RFS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_SMM": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SMM Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_SSD": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_SSD Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_TOSCANA": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_TOSCANA Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_TMS": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_TMS Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_ITIME": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_ITIME Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_VCP": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_VCP Schema",
+                    "examples": [
+                        "0"
+                    ]
+                },
+                "cnt_BRQ": {
+                    "type": "string",
+                    "default": "",
+                    "title": "The cnt_BRQ Schema",
+                    "examples": [
+                        "0"
+                    ]
+                }
+            },
+            "examples": [{
+                "cnt_TRA": "45",
+                "cnt_STA": "32",
+                "cnt_SAINFRA": "14",
+                "cnt_GCS": "11",
+                "cnt_GIS": "11",
+                "cnt_SATECH": "8",
+                "cnt_TAS": "7",
+                "cnt_SR": "5",
+                "cnt_TARMAC": "1",
+                "cnt_LMS": "0",
+                "cnt_SPS": "0",
+                "cnt_PIPB": "0",
+                "cnt_SM": "0",
+                "cnt_AAM": "0",
+                "cnt_AAI": "0",
+                "cnt_CO": "0",
+                "cnt_CA": "0",
+                "cnt_CVM": "0",
+                "cnt_CCL": "0",
+                "cnt_ACT": "0",
+                "cnt_ESS": "0",
+                "cnt_ESOP": "0",
+                "cnt_EMS": "0",
+                "cnt_FM": "0",
+                "cnt_IRFS": "0",
+                "cnt_IFC": "0",
+                "cnt_ODC": "0",
+                "cnt_PMS": "0",
+                "cnt_PG": "0",
+                "cnt_RAS": "0",
+                "cnt_RFS": "0",
+                "cnt_SMM": "0",
+                "cnt_SSD": "0",
+                "cnt_TOSCANA": "0",
+                "cnt_TMS": "0",
+                "cnt_ITIME": "0",
+                "cnt_VCP": "0",
+                "cnt_BRQ": "0"
+            }]
+        }
+    },
+    "examples": [{
+        "d": {
+            "cnt_TRA": "45",
+            "cnt_STA": "32",
+            "cnt_SAINFRA": "14",
+            "cnt_GCS": "11",
+            "cnt_GIS": "11",
+            "cnt_SATECH": "8",
+            "cnt_TAS": "7",
+            "cnt_SR": "5",
+            "cnt_TARMAC": "1",
+            "cnt_LMS": "0",
+            "cnt_SPS": "0",
+            "cnt_PIPB": "0",
+            "cnt_SM": "0",
+            "cnt_AAM": "0",
+            "cnt_AAI": "0",
+            "cnt_CO": "0",
+            "cnt_CA": "0",
+            "cnt_CVM": "0",
+            "cnt_CCL": "0",
+            "cnt_ACT": "0",
+            "cnt_ESS": "0",
+            "cnt_ESOP": "0",
+            "cnt_EMS": "0",
+            "cnt_FM": "0",
+            "cnt_IRFS": "0",
+            "cnt_IFC": "0",
+            "cnt_ODC": "0",
+            "cnt_PMS": "0",
+            "cnt_PG": "0",
+            "cnt_RAS": "0",
+            "cnt_RFS": "0",
+            "cnt_SMM": "0",
+            "cnt_SSD": "0",
+            "cnt_TOSCANA": "0",
+            "cnt_TMS": "0",
+            "cnt_ITIME": "0",
+            "cnt_VCP": "0",
+            "cnt_BRQ": "0"
+        }
+    }]
+}
